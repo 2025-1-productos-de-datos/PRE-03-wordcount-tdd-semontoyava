bf0efd6407504f5207396d6876208611fa954910
semontoyava <semontoyava@unal.edu.co>
2025-05-31 11:42:52 -0500

    teste

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,25 +1,23 @@
-# obtain a list of files in the input directoryy
-import os
+# ejemplo del caso de uso
+#
 
-files_in_input_dir = os.listdir("data/input/")
-files_in_input_dir
+## wordcount.py
 
-# count the frequency of the words in the files in the input directory
-counter = {}
-for filename in files_in_input_dir:
-    with open("data/input/" + filename) as f:
-        for l in f:
-            for w in l.split():
-                w = w.lower().strip(",.!?")
-                counter[w] = counter.get(w, 0) + 1
+import argparse
+import sys
 
-# create the directory output/ if it doesn't exist
-if not os.path.exists("data/output"):
-    os.makedirs("data/output")
+from homework.src._internals.count_words import count_words
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
-# save the results using tsv format
-with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-    for key, value in counter.items():
-        # write the key and value to the file
-        f.write(f"{key}\t{value}\n")
+def main():
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
